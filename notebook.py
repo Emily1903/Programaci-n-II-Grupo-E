48a3bf12428fb9d2d86adb6e28e00f6de88cf375
Emily1903 <emilyaguilarroca361@gmail.com>
2025-08-29 08:34:26 -0400

    Add files via upload

---
+++ notebook.py
@@ -0,0 +1,74 @@
+#importación de libreria
+import tkinter as tk 
+from tkinter import ttk, messagebox
+#Crear una ventana principal
+ventana_principal=tk.Tk()
+ventana_principal.title("Libro de Pacientes y Doctores")
+ventana_principal.geometry("400x600")
+
+#Crear contenedor Notebook(pestañas)
+pestañas=ttk.Notebook(ventana_principal)
+#Crear frames(uno por pestaña)
+frame_pacientes=ttk.Frame(pestañas)
+#Agregar pestañas al Notebook
+pestañas.add(frame_pacientes,text="Pacientes")
+
+#Mostrar las pestañas en la ventana
+pestañas.pack(expand=True,fill="both") #fill=relleno, both para que se muestre en todo el espacio
+
+#Nombre
+labelNombre=tk.Label(frame_pacientes,text="Nombre Completo: ")
+labelNombre.grid(row=0,column=0,sticky="w",padx=5,pady=5)
+nombreP=tk.Entry(frame_pacientes)
+nombreP.grid(row=0,column=1,sticky="w",padx=5,pady=5)
+
+#Fecha de nacimiento
+labelFechaN=tk.Label(frame_pacientes,text="Fecha de Nacimiento: ")
+labelFechaN.grid(row=1,column=0,sticky="w",padx=5,pady=5)
+fechaN=tk.Entry(frame_pacientes)
+fechaN.grid(row=1,column=1,sticky="w",padx=5,pady=5)
+
+#Edad(readonly)
+labelEdad=tk.Label(frame_pacientes,text="Edad: ")
+labelEdad.grid(row=2,column=0,sticky="w",padx=5,pady=5)
+edadP=tk.Entry(frame_pacientes,state="readonly")
+edadP.grid(row=2,column=1,sticky="w",padx=5,pady=5)
+
+#Género
+labelGenero=tk.Label(frame_pacientes,text="Género: ")
+labelGenero.grid(row=3,column=0,sticky="w",padx=5,pady=5)
+genero=tk.StringVar()
+genero.set("Masculino") #Valor por defecto
+radioMasculino=ttk.Radiobutton(frame_pacientes,text="Masculino",variable=genero,value="Masculino")
+radioMasculino.grid(row=3,column=1,sticky="w",padx=5)
+radioFemenino=ttk.Radiobutton(frame_pacientes,text="Femenino",variable=genero,value="Femenino")
+radioFemenino.grid(row=4,column=1,sticky="w",padx=5)
+
+#Grupo Sanguíneo
+labelGrupoSanguineo=tk.Label(frame_pacientes,text="Grupo Sanguíneo: ")
+labelGrupoSanguineo.grid(row=5,column=0,sticky="w",padx=5,pady=5)
+grupoSanguineo=tk.Entry(frame_pacientes,text="Grupo Sanguíneo")
+grupoSanguineo.grid(row=5,column=1,padx=5,pady=5,sticky="w")
+
+#Tipo de Seguro
+labelTipoSeguro=tk.Label(frame_pacientes,text="Tipo de seguro: ")
+labelTipoSeguro.grid(row=6,column=0,sticky="w",padx=5,pady=5)
+tipo_seguro=tk.StringVar()
+tipo_seguro.set("Público")#Valor por defecto
+comboTipoSeguro=ttk.Combobox(frame_pacientes,values=["Público","Privado","Ninguno"],textvariable=tipo_seguro)
+comboTipoSeguro.grid(row=6,column=1,sticky="w",padx=5,pady=5)
+
+#Centro Médico
+labelCentroMedico=tk.Label(frame_pacientes,text="Centro de salud: ")
+labelCentroMedico.grid(row=7,column=0,sticky="w",padx=5,pady=5)
+centro_medico=tk.StringVar()
+centro_medico.set("Hospital Central") #Valos por defecto
+comboCentroMedico=ttk.Combobox(frame_pacientes,values=["Hospital Central","Clínica Norte","Centro Sur"],textvariable=centro_medico)
+comboCentroMedico.grid(row=7,column=1,sticky="w",padx=5,pady=5)
+
+#Pestaña doctores
+frame_doctores=ttk.Frame(pestañas)
+pestañas.add(frame_doctores,text="Doctores")
+pestañas.pack(expand=True,fill="both")
+
+ventana_principal.mainloop()
